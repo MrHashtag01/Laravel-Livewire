e7e8528d1a9d3cd0b97dbae7f0d183ba929d2ab1
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-06-08 16:07:17 +0100

    Added modal in posts.blade.php file, modified delete functions and included Jquery and Javascript libraries in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,12 @@
 <head>
     <title>Laravel Livewire CRUD - ItSolutionStuff.com</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     @livewireStyles
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-3">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
